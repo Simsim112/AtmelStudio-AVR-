91687feaa9645d328caa128c41af2eefb217c514
Simsim112 <dkfmtldks2@gmail.com>
2022-06-17 17:31:43 +0900

    220617 AVR 공부

--- FND1.c
+++ FND1.c
@@ -13,39 +13,41 @@ int main()
 	DDRD = 0x00;
 	DDRA = 0xFF;
 	
+	//0~7까지의 숫자(16진수)
+	int arr[] = {0xFD, 0x61, 0xDB, 0xF3, 0x67 , 0xB7, 0x3F, 0xE5};
 	while(1)
 	{
 		if(PIND == 0x80)
 		{
-			PORTA =	0xFD; //0b 1111 1101
+			PORTA =	arr[0]; //0b 1111 1101
 		}
 		else if(PIND == 0x40)
 		{
-			PORTA = 0x61; //0b 0110 0001
+			PORTA = arr[1]; //0b 0110 0001
 		}
 		else if(PIND == 0x20)
 		{
-			PORTA = 0xDB; //0b 1101 1011
+			PORTA = arr[2]; //0b 1101 1011
 		}
 		else if(PIND == 0x10)
 		{
-			PORTA = 0xF3; //0b 1111 0011
+			PORTA = arr[3]; //0b 1111 0011
 		}
 		else if(PIND == 0x08)
 		{
-			PORTA = 0x67; //0b 0110 0111
+			PORTA = arr[4]; //0b 0110 0111
 		}
 		else if(PIND == 0x04)
 		{
-			PORTA = 0xB7; //0b 1011 0111
+			PORTA = arr[5]; //0b 1011 0111
 		}
 		else if(PIND == 0x02)
 		{
-			PORTA = 0x3F; //0b 0011 1111
+			PORTA = arr[6]; //0b 0011 1111
 		}
 		else if(PIND == 0x01)
 		{
-			PORTA = 0xE5; //0b 1110 0101
+			PORTA = arr[7]; //0b 1110 0101
 		}
 	}
 	return 0;
