665096432b34399d87092fd8653d1766f7e95238
Simsim112 <dkfmtldks2@gmail.com>
2022-06-17 17:10:01 +0900

    220617 AVR 공부

---
+++ FND1.c
@@ -0,0 +1,53 @@
+﻿/*
+ * FND1.c
+ *
+ * Created: 2022-06-17 오후 4:21:32
+ *  Author: PKNU
+ */ 
+#define F_CPU 7432800UL
+#include<avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRD = 0x00;
+	DDRA = 0xFF;
+	
+	while(1)
+	{
+		if(PIND == 0x80)
+		{
+			PORTA =	0xFD; //0b 1111 1101
+		}
+		else if(PIND == 0x40)
+		{
+			PORTA = 0x61; //0b 0110 0001
+		}
+		else if(PIND == 0x20)
+		{
+			PORTA = 0xDB; //0b 1101 1011
+		}
+		else if(PIND == 0x10)
+		{
+			PORTA = 0xF3; //0b 1111 0011
+		}
+		else if(PIND == 0x08)
+		{
+			PORTA = 0x67; //0b 0110 0111
+		}
+		else if(PIND == 0x04)
+		{
+			PORTA = 0xB7; //0b 1011 0111
+		}
+		else if(PIND == 0x02)
+		{
+			PORTA = 0x3F; //0b 0011 1111
+		}
+		else if(PIND == 0x01)
+		{
+			PORTA = 0xE5; //0b 1110 0101
+		}
+	}
+	return 0;
+	
+}
---
+++ LED2.c
@@ -0,0 +1,32 @@
+﻿/*
+ * LED2.c
+ *
+ * Created: 2022-06-17 오후 1:54:32
+ *  Author: PKNU
+ */ 
+
+#define F_CPU 7432800
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRA = 0xFF;
+	PORTA = 0x01;
+	while(1)
+	{
+		_delay_ms(1000);
+		PORTA = PORTA << 1;
+		if(PORTA == 0x80)
+		{
+			for(int i =0; i <7;i++)
+			{
+				_delay_ms(1000);
+				PORTA = PORTA >> 1;
+			}
+		}
+
+	}
+
+	return 0;
+}
